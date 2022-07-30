63f3543b8c0b9a60a590c41ddbdc42839c0dd6bd
新 地引 <jibikiarata@gmail.com>
2022-07-31 02:03:24 +0900

    fix about logo in about page

--- HP/index/index.js
+++ HP/index/index.js
@@ -1,4 +1,4 @@
-const img_src= [ "slide-img01.jpg", "slide-img02.jpg", "slide-img03.jpg", "slide-img04.jpg", "slide-img05.jpg", "slide-img06.jpg", "slide-img-07.jpg"];
+const img_src= [ "slide-img/slide-img01.jpg", "slide-img/slide-img02.jpg", "slide-img/slide-img03.jpg", "slide-img/slide-img04.jpg", "slide-img/slide-img05.jpg", "slide-img/slide-img06.jpg", "slide-img/slide-img-07.jpg"];
 let num = -1;
 function slide_time() {
     if (num === 5) {
