17522d88872ff711fab34b16f80f26e44c3c6683
新 地引 <jibikiarata@gmail.com>
2022-07-30 20:28:28 +0900

    make slide show in "index.js"

---
+++ HP/index/index.js
@@ -0,0 +1,11 @@
+const img_src= [ "slide-img01.jpg", "slide-img02.jpg", "slide-img03.jpg", "slide-img04.jpg", "slide-img05.jpg", "slide-img06.jpg", "slide-img-07.jpg"];
+let num = -1;
+function slide_time() {
+    if (num === 5) {
+        num=0;
+    } else {
+        num++;
+    }
+    document.getElementById("slide-content").src = img_src[num];
+}
+setInterval(slide_time,1500);
